44eee77dcb3c13e3c25601154aa5601ea94e552a
babiloneo <cronica_20@hotmail.com>
2017-05-23 00:16:44 -0600

    calendarios y botones de inicio

--- app/src/main/java/itesloscabos/com/hotelapp/IndexActivity.java
+++ app/src/main/java/itesloscabos/com/hotelapp/IndexActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -36,6 +37,10 @@ public class IndexActivity extends AppCompatActivity {
         setContentView(R.layout.activity_index);
         listaHoteles=(ListView)findViewById(R.id.lista_hoteles);
         obtenerDatosIndex();
+        TextView fechas=(TextView)findViewById(R.id.txt_fechas);
+        TextView persona=(TextView)findViewById(R.id.textView10);
+        fechas.setText(getIntent().getStringExtra("fechas"));
+        persona.setText(getIntent().getStringExtra("personas"));
     }
 
     private void obtenerDatosIndex(){
@@ -44,7 +49,6 @@ public class IndexActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        // String Authorization = "bearer SqEB0uHh9_1vTNHPIAE8DeJfmrQx4mbY7fqK1VAmDta1SNXQ-hMMEjbwxu3uMhUMKW0cmYH6wkaM3TVqd2b9Wl8G91s-SIQtApTIBtVVrQf0_gtHMTgjn0kZUjpzXaYORCu9ThfNEVj482e_230RRu1OeLzUImnzDC-1JduNSp58Tr8cyRLrzTtGydO7AgNdSSKaXpDsAiZp7umz5d18goHfuEvoIYQIOjmAnfnQDfhKbNmnx_lGj32oeswOIXdc2upoC1icCDyCavVyRjDB0AC2K0fWVqZ4MmlYGO2gMCYdpGskC-U4YZYjTbBdcE3FcvdUJ8UeHcRpeghS8_Q9nOi13hCUgOk_r92cF3ii2dHrTyIOTGH8KdzabK8PkWdza38mkgC7SAFe9yBNqvpvt5s1J3JvGzK-DyGTX7Yj1NEzOTqK2kJM5zrKg96US9fGHrmznsn-iSE2SuXOjcBhiiBWXcIz1xJSi2Xej8Ys2WMpYnuwGR7zzfm9zGIMPdRuqeG9XJusnBE4YAnNfZ5rTw";
         String Environment ="TEST";
         String iata = "VER";
         String refPoint = getIntent().getStringExtra("destino");
@@ -55,7 +59,6 @@ public class IndexActivity extends AppCompatActivity {
         String children = getIntent().getStringExtra("ninos");
         String ages = "1,3";
         Boolean go =true;
-        //String rojo='proenfjki';
 
         Service service = retrofit.create(Service.class);
         service.ObtenerListaHotel("Bearer " + AppState.accessToken,Environment,iata,refPoint,checkIn,checkOut,rooms,adults,children).enqueue(new Callback<Hotel>() {
@@ -68,21 +71,11 @@ public class IndexActivity extends AppCompatActivity {
 
                 if(response.isSuccessful()){
                     Log.i(TAG,"Hoteles"+x+" "+y+" "+z+" "+a);
-                    //  Hotel result = response.body();
+
                     indexResult =response.body().getResult();
                     adapter=new IndexAdapter(getApplicationContext(),R.layout.listview_index,indexResult);
                     listaHoteles.setAdapter(adapter);
-/*
-                    for(int i=0;i<indexResult.size();i++){
-                        hotelResult p =indexResult.get(i);
-                        Log.i(TAG,"Hoteles"+p.getCountry()+","+p.getCity()+","+p.getName());
-                        ArrayList<indexImagenes> indexImg=p.getImages();
 
-                        for(int j=0;j<indexImg.size();j++){
-                            indexImagenes q=indexImg.get(j);
-                            Log.i(TAG,"imagenes"+q.getKeyword()+","+q.getDescription()+","+q.getUrl());
-                        }
-                    }*/
                 }else{
                     Log.e(TAG, "onResponse: "+response.errorBody());
                 }
--- app/src/main/java/itesloscabos/com/hotelapp/InicioActivity.java
+++ app/src/main/java/itesloscabos/com/hotelapp/InicioActivity.java
@@ -1,16 +1,25 @@
 package itesloscabos.com.hotelapp;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
 
 import itesloscabos.com.hotelapp.Models.AppState;
 import itesloscabos.com.hotelapp.Models.Hotel;
@@ -22,18 +31,22 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import itesloscabos.com.hotelapp.HotelAPi.Service;
-public class InicioActivity extends AppCompatActivity {
+public class InicioActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private Retrofit retrofit;
     private static final String TAG = "PRUEBA";
     Button tarifas;
     private Button secion;
     private EditText destino;
-    private EditText llegada;
-    private EditText salida;
+    private TextView llegada;
+    private TextView salida;
     private TextView cuartos;
     private TextView Adultos;
     private TextView ninos;
+    TextView calendario;
+    String txt_llegada,txt_salida;
+
+    ImageView menos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,15 +55,151 @@ public class InicioActivity extends AppCompatActivity {
         secion=(Button)findViewById(R.id.textView);
         tarifas=(Button) findViewById(R.id.ver_tarifas);
         destino=(EditText)findViewById(R.id.txt_destino);
-        llegada=(EditText)findViewById(R.id.txt_llegada);
-        salida= (EditText)findViewById(R.id.txt_salida);
+        llegada=(TextView) findViewById(R.id.txt_llegada);
+        salida= (TextView) findViewById(R.id.txt_salida);
         cuartos=(TextView)findViewById(R.id.txt_habitacion);
         Adultos=(TextView)findViewById(R.id.txt_adultos);
         ninos=(TextView)findViewById(R.id.txt_ninos);
 
        // ObtenerDatosLogin();
+        add_rem();
         iniciar_secion();
         mostrarTarifas();
+    }
+
+    public void calendario1(View view){
+        calendario=(TextView)findViewById(R.id.txt_llegada);
+        datePicker(view);
+    }
+
+    public void calendario2(View view){
+        calendario=(TextView)findViewById(R.id.txt_salida);
+        datePicker(view);
+    }
+
+    public void datePicker(View view){
+        DatePickerFragment fragment = new  DatePickerFragment();
+        fragment.show(getSupportFragmentManager(),"Date");
+    }
+
+    public void setDate(final Calendar calender){
+        final DateFormat dateform= DateFormat.getDateInstance(DateFormat.MEDIUM);
+        calendario.setText(dateform.format(calender.getTime()));
+    }
+    public void add_rem(){
+        ImageView menos_A=(ImageView)findViewById(R.id.ha_rem);
+        ImageView mas_A=(ImageView)findViewById(R.id.ha_add);
+        ImageView men_Ad=(ImageView)findViewById(R.id.ad_rem);
+        ImageView mas_Ad=(ImageView)findViewById(R.id.ad_add);
+        ImageView men_ni=(ImageView)findViewById(R.id.ni_rem);
+        ImageView mas_ni=(ImageView)findViewById(R.id.ni_add);
+
+        menos_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    int x =Integer.parseInt(((EditText)findViewById(R.id.txt_habitacion)).getText().toString());
+                    if(x>0){
+                        x--;
+                    }
+                    ((EditText)findViewById(R.id.txt_habitacion)).setText(String.valueOf(x));
+
+            }
+        });
+        mas_A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x =Integer.parseInt(((EditText)findViewById(R.id.txt_habitacion)).getText().toString());
+                if(x>=0 && x<=10){
+                    x++;
+                }
+                ((EditText)findViewById(R.id.txt_habitacion)).setText(String.valueOf(x));
+            }
+
+        });
+
+        men_Ad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x =Integer.parseInt(((EditText)findViewById(R.id.txt_adultos)).getText().toString());
+                if(x>0){
+                    x--;
+                }
+                ((EditText)findViewById(R.id.txt_adultos)).setText(String.valueOf(x));
+
+            }
+        });
+
+        mas_Ad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x =Integer.parseInt(((EditText)findViewById(R.id.txt_adultos)).getText().toString());
+                if(x>=0 && x<=10){
+                    x++;
+                }
+                ((EditText)findViewById(R.id.txt_adultos)).setText(String.valueOf(x));
+            }
+        });
+
+        men_ni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x =Integer.parseInt(((EditText)findViewById(R.id.txt_ninos)).getText().toString());
+                if(x>0){
+                    x--;
+                }
+                ((EditText)findViewById(R.id.txt_ninos)).setText(String.valueOf(x));
+
+            }
+        });
+
+        mas_ni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x =Integer.parseInt(((EditText)findViewById(R.id.txt_ninos)).getText().toString());
+                if(x>=0 && x<=10){
+                    x++;
+                }
+                ((EditText)findViewById(R.id.txt_ninos)).setText(String.valueOf(x));
+            }
+        });
+    }
+
+    public String mes(String cadena){
+        String []arre;
+        arre=cadena.split(" ");
+        String mes= arre[1].replace(".","");
+        String nuevomes="";
+        if(mes=="ene"){
+            nuevomes="01";
+        }else if(mes=="feb"){
+            nuevomes="02";
+        }else if(mes=="feb"){
+            nuevomes="02";
+        }else if(mes=="mar"){
+            nuevomes="03";
+        }else if(mes=="abr"){
+            nuevomes="04";
+        }else if(mes=="may"){
+            nuevomes="05";
+        }else if(mes=="jun"){
+            nuevomes="06";
+        }else if(mes=="jul"){
+            nuevomes="07";
+        }else if(mes=="ago"){
+            nuevomes="08";
+        }else if(mes=="sept"){
+            nuevomes="09";
+        }else if(mes=="oct"){
+            nuevomes="10";
+        }else if(mes=="nov"){
+            nuevomes="11";
+        }else if(mes=="dic"){
+            nuevomes="12";
+        }
+
+        String fecha=arre[2]+nuevomes+arre[0];
+        return fecha;
     }
 
     public void mostrarTarifas(){
@@ -59,6 +208,11 @@ public class InicioActivity extends AppCompatActivity {
         tarifas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String llega,sal;
+                llega=mes(llegada.getText().toString());
+                sal=mes(salida.getText().toString());
+                String fechas=llegada.getText().toString()+"-"+salida.getText().toString();
+                String personas=Adultos.getText().toString()+" Adultos "+ninos.getText().toString()+" Niños";
                 if(AppState.accessToken=="")
                 {
                     Toast.makeText(getApplicationContext(), "Inicie su secion", Toast.LENGTH_SHORT).show();
@@ -66,11 +220,13 @@ public class InicioActivity extends AppCompatActivity {
                     Intent nextViewIndex = new Intent(InicioActivity.this,IndexActivity.class);
                     Bundle datos = new Bundle();
                     datos.putString("destino",destino.getText().toString());
-                    datos.putString("llegada",llegada.getText().toString());
-                    datos.putString("salida",salida.getText().toString());
+                    datos.putString("llegada",llega);
+                    datos.putString("salida",sal);
                     datos.putString("cuartos",cuartos.getText().toString());
                     datos.putString("adultos",Adultos.getText().toString());
                     datos.putString("ninos",ninos.getText().toString());
+                    datos.putString("fechas",fechas);
+                    datos.putString("personas",personas);
                     nextViewIndex.putExtras(datos);
                     startActivity(nextViewIndex);
                 }
@@ -140,4 +296,24 @@ public class InicioActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal = new GregorianCalendar(year,month,day);
+        setDate(cal);
+    }
+
+    public static class DatePickerFragment extends DialogFragment{
+
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+
+            final Calendar c =Calendar.getInstance();
+            int year =c.get(Calendar.YEAR);
+            int month =c.get(Calendar.MONTH);
+            int day =c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(),
+            (DatePickerDialog.OnDateSetListener)
+                    getActivity(),year,month,day);
+        }
+    }
 }
